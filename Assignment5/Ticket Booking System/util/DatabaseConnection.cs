8321baca25be42c099ec902dc410d934561d92ed
naveen0814 <161302822+naveen0814@users.noreply.github.com>
2025-04-25 21:03:58 +0530

    Add files via upload

---
+++ Assignment5/Ticket Booking System/util/DatabaseConnection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace TicketBookingSystem
+{
+    public class DatabaseConnection
+    {
+        private static string connectionString = "Server=JARVIS-LAPTOP;Database=TicketBookingSystem;Trusted_Connection=True;";
+
+        public static SqlConnection GetConnection()
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            return connection;
+        }
+    }
+}
+
